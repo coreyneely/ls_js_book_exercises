09b7bb63adf97bc2d5f4029dbff4d7e784d7b110
coreyneely <kamakazi42@gmail.com>
2021-04-01 11:27:09 -0400

    functions_exercises

---
+++ functions_exercises/exercise_04_.js
@@ -0,0 +1,14 @@
+/** 4. What does the following code log to the console?  */
+
+function scream(words) {
+  words = words + '!!!!';
+  return;
+  console.log(words);
+}
+
+scream('Yipeee');
+
+// solution = It doesn't log anything. The return on line 3 terminates the function before it can log anything.
+
+
+
---
+++ functions_exercises/exercise_05_.js
@@ -0,0 +1,10 @@
+/** 5. What does the following code log to the console?  */
+
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');
+
+// solution = This program also doesn't log anything. The function returns a value, Yipeee!!!!, but it doesn't do anything with it. In particular, it doesn't write it to the console.
+
